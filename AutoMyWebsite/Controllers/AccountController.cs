4e0d1b0017fb46d283ba27ae769fe48a3512e806
MoSe <123MoSe@users.noreply.github.com>
2020-01-31 08:49:15 -0800

    sadasd
    Co-Authored-By: lukito2004 <lukito2004@users.noreply.github.com>

--- AutoMyWebsite/Controllers/AccountController.cs
+++ AutoMyWebsite/Controllers/AccountController.cs
@@ -26,6 +26,8 @@ namespace AutoMyWebsite.Controllers
         private readonly SignInManager<Account> _signInManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly string hey = "hi wtff";
+        private readonly string xvich = "hi wtff";
+
 
         private readonly IMapper _mapper;
 
